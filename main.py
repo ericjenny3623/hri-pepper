1b7056cb5ec6b380d2278ed56226ac46237e3ecd
ericjenny3623 <erc.hakawa@gmail.com>
2022-03-29 13:46:35 -0400

    Cleaned

---
+++ main.py
@@ -0,0 +1,19 @@
+numRounds = 20
+#control
+for i in range(numRounds):
+    #voice command to ask "do you choose even or odd"
+    option = raw_input("Press 1=even or 2=odd")
+    #voice command to count down 3, 2, 1
+    if option == "1":
+        pass
+        #go to motion 1
+    else:
+        pass
+        #go to motion 2
+    option = raw_input("Press 1=win or 2=lose")
+    if option == "1":
+        pass
+        #voice command "Yes I win"
+    else:
+        pass
+        #voice command "Aw you win"
